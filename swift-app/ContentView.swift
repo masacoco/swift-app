13579c905e5a1f70c724a8b0df30fd02316ad720
masacoco <masaonakama0329@gmail.com>
2021-11-30 14:03:09 +0900

    first commit

---
+++ swift-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift-app
+//
+//  Created by Masao Nakama on 11/30/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift-app/swift_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_appApp.swift
+//  swift-app
+//
+//  Created by Masao Nakama on 11/30/21.
+//
+
+import SwiftUI
+
+@main
+struct swift_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
