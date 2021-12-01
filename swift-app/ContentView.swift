6c35131767d99bcfafefd6614ad0e05fb6aa3a29
masacoco <masaonakama0329@gmail.com>
2021-12-01 19:39:47 +0900

    [add]list

--- swift-app/ContentView.swift
+++ swift-app/ContentView.swift
@@ -21,7 +21,21 @@ struct ContentView: View {
                     }
                     .submitLabel(.search)
                     .padding()
+            
+            List(okashiDataList.okashiList){ okashi in
+                HStack{
+                    AsyncImage(url: okashi.image){ image in
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(height: 40)
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    Text(okashi.name)
                 }
+            }
+        }
     }
 }
 
