e21767178df5096043df321399708b3b57c49841
masacoco <masaonakama0329@gmail.com>
2021-12-01 18:22:37 +0900

    [add]textfield

--- swift-app/ContentView.swift
+++ swift-app/ContentView.swift
@@ -8,9 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var okashiDataList = OkashiData()
+    @State var inputText = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            TextField("キーワード", text: $inputText, prompt: Text("キーワードを入力してください"))
+                    .onSubmit {
+                        Task{
+                            await okashiDataList.searchOkashi(keyword: inputText)
+                        }
+                    }
+                    .submitLabel(.search)
+                    .padding()
+                }
     }
 }
 
---
+++ swift-app/OkashiData.swift
@@ -0,0 +1,14 @@
+//
+//  OkashiData.swift
+//  swift-app
+//
+//  Created by Masao Nakama on 12/1/21.
+//
+
+import Foundation
+//お菓子検索用クラス
+class OkashiData: ObservableObject{
+    func searchOkashi(keyword: String) async {
+        print(keyword)
+    }
+}
