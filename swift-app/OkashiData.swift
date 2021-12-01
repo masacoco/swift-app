46076723a9604228d16b03e53147e34bb8ba96c1
masacoco <masaonakama0329@gmail.com>
2021-12-01 18:33:52 +0900

    [add]URL encoder

--- swift-app/OkashiData.swift
+++ swift-app/OkashiData.swift
@@ -10,5 +10,19 @@ import Foundation
 class OkashiData: ObservableObject{
     func searchOkashi(keyword: String) async {
         print(keyword)
+        
+//        お菓子の検索URLエンコード
+        guard let keyword_encode = keyword.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+            return
+        }
+
+//        リクエストURL組み立て
+        guard let req_url = URL(string: "https://sysbird.jp/toriko/api/?apikey=guest&format=json&keyword=\(keyword_encode)&max=10&order=r") else {
+            return
+        }
+        print(req_url)
     }
 }
+
+
+
